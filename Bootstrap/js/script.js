9892aeb829a5fccc48a67a8c46e0be06f4b2fa0f
loemkrutoi <loemloh@gmail.com>
2025-02-24 23:44:14 +0200

    added some anims

--- Bootstrap/js/script.js
+++ Bootstrap/js/script.js
@@ -1,19 +1,11 @@
-// // Example starter JavaScript for disabling form submissions if there are invalid fields
-// let baton = document.querySelector('#baton');
+function InfoBtn(target){
+    target.classList.toggle('clicked-info');
+}
 
-// baton.addEventListener('click',function(){
-//     // Fetch all the forms we want to apply custom Bootstrap validation styles to
-//     const forms = document.querySelectorAll('.needs-validation')
-  
-//     // Loop over them and prevent submission
-//     Array.from(forms).forEach(form => {
-//       form.addEventListener('submit', event => {
-//         if (!form.checkValidity()) {
-//           event.preventDefault()
-//           event.stopPropagation()
-//         }
-  
-//         form.classList.add('was-validated')
-//       }, false)
-//     })
-// })
+let invisible = document.getElementById('invisible');
+function AppearAfterFirstSlide(){
+    if(window.scrollY < 600){
+        invisible.classList.add('invisible');
+    }
+    else invisible.classList.remove('invisible');
+}
