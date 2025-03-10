8422d1a296ae7f5882564fc0609c594ebb4d7278
loemkrutoi <loemloh@gmail.com>
2025-03-10 02:54:42 +0200

    reviews added

--- Bootstrap/js/script.js
+++ Bootstrap/js/script.js
@@ -3,9 +3,19 @@ function InfoBtn(target){
 }
 
 let goUpBtn = document.getElementById('goUpBtn');
+let firstSlide = document.getElementById('firstSlide');
+let firstSlideText = document.getElementById('firstSlideText');
+
 function AppearAfterFirstSlide(){
     if(window.scrollY < 600){
-        goUpBtn.style.animation = '0.3s linear 1 GoUpBtnAnimOut forwards'
+        goUpBtn.style.animation = '0.3s linear 1 GoUpBtnAnimOut forwards';
+        firstSlide.style.animation = '2s ease-in-out HandAnim forwards';
+        firstSlideText.style.animation = '3s ease-in-out TextAnim forwards';
     }
-    else goUpBtn.style.animation = '0.3s linear GoUpBtnAnimIn forwards';
+    else
+    {
+        goUpBtn.style.animation = '0.3s linear GoUpBtnAnimIn forwards';
+        firstSlide.style.animation = '0.5s ease-in-out OpacityUp forwards';
+        firstSlideText.style.animation = '0.5s ease-in-out OpacityUp forwards';
+    }
 }
