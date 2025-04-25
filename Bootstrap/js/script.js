a28c88e2bb96fed18112d1eb1096cc3b221590c6
loemkrutoi <loemloh@gmail.com>
2025-04-25 07:39:42 +0200

    responsive

--- Bootstrap/js/script.js
+++ Bootstrap/js/script.js
@@ -1,21 +1,17 @@
 let goUpBtn = document.getElementById('goUpBtn');
-// let firstSlide = document.getElementById('firstSlide');
-// let firstSlideText = document.getElementById('firstSlideText');
-// let shards = document.getElementById('shards');
 
 function AppearAfterFirstSlide(){
-    if(pageYOffset < document.documentElement.clientHeight){
+    if (window.innerWidth > 767){
+        if(pageYOffset < document.documentElement.clientHeight){
+       
         goUpBtn.style.animation = '0.3s linear 1 GoUpBtnAnimOut forwards';
-        // firstSlide.style.animation = '2s ease-in-out HandAnim forwards';
-        // firstSlideText.style.animation = '3s ease-in-out TextAnim forwards';
+        }
+        else
+        {
+            goUpBtn.style.animation = '0.3s linear GoUpBtnAnimIn forwards';
+        }
     }
-    else
-    {
-        goUpBtn.style.animation = '0.3s linear GoUpBtnAnimIn forwards';
-        // firstSlide.style.animation = '0.5s ease-in-out OpacityUp forwards';
-        // firstSlideText.style.animation = '0.5s ease-in-out OpacityUp forwards';
-        // shards.style.animation = '0.5s ease-in-out OpacityDown forwards';
-    }
+    
 }
 
 let reviews = document.querySelectorAll('.review');
